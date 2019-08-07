33010e203ca969c252753d5bd02c62931cf9a357
winsmatter <34270273+winsmatter@users.noreply.github.com>
2019-08-07 12:38:02 +0530

    jgjgj

---
+++ story_content/user.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5p0ihvh8YDR":
+        Script1();
+        break;
+      case "5XR46Zlpk2D":
+        Script2();
+        break;
+  }
+}
+var a;
+function init(val)
+{
+	
+	//var player=GetPlayer();
+    //player.SetVar("dummy_var3", val);
+	a= val;
+}
+function Script1()
+{
+  // var player = GetPlayer();
+//var tokenid = player.GetVar("dummy_var3");
+//alert(a);
+
+
+}
+
+function Script2()
+{
+ // var player = parent.GetPlayer();
+//var tokenid = player.GetVar("dummy_var3");
+
+var passURL = "Assessment/index.html?tokenid ="+a;
+window.open(passURL,"_self");
+}
+
